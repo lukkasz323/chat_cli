01a5fe9a03c6bc2a97d60a53afdf93d3a91d7318
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-05-12 04:43:44 +0200

    Add files via upload

---
+++ chat-client.py
@@ -0,0 +1,6 @@
+import socket
+
+if __name__ == '__main__':
+    client = socket.socket()
+    print('Client: ')
+    # print(socket.create_connection())
---
+++ chat-server.py
@@ -0,0 +1,8 @@
+import socket
+
+if __name__ == '__main__':
+    print("Starting server...")
+    print(socket.getnameinfo)
+    print("Server started!")
+    # Awaiting clients
+    print("Server stopped.")
