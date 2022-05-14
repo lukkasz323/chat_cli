82aab3e132f7544a691f0197f7cc63bfddf624c2
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-05-14 22:50:01 +0200

    Change general functionality

--- chat_client.py
+++ chat_client.py
@@ -1,32 +1,38 @@
 import socket
 
-def exception(msg):
-    print(f'\nEXCEPTION: {msg}\n')
+def exception(e):
+    print(f'\nEXCEPTION: {e}\n')
 
 def chat(prefix=''):
     inp = input(f'{prefix}> ')
-    encoded = inp.encode()
-    client.sendall(encoded)
+    inp_bytes = inp.encode()
+    client.sendall(inp_bytes)
+
+def receive():
+    msg_bytes= client.recv(64)
 
 if __name__ == '__main__':
-    HOST = 'localhost'
     PORT = 50001
+    HOST = 'localhost'
+    name = 'Marek'
+    name_bytes = name.encode()
     attempt = 1
-
+    xd = 0
+    
+    print('[CLIENT]\n')
     while True:
         print(f'Connecting to ({HOST}, {PORT})... [{attempt}]')
         try:
             with socket.create_connection((HOST, PORT)) as client:
                 print('Connected to', client.getpeername())
-                try:
-                    print('Please input your name:')
-                    chat('Name')
-                    print('Connection closed.\n')
-                except Exception as msg:
-                    exception(msg)
-                break
-        except Exception as msg:
-            exception(msg)
+                client.sendall(name_bytes)
+                while True:
+                    chat()
+                    break
+            print('Connection closed.\n')
+            break
+        except Exception as e:
+            exception(e)
             if attempt < 10:
                 attempt += 1
             else:
--- chat_server.py
+++ chat_server.py
@@ -2,24 +2,36 @@ import socket
 import time
 
 if __name__ == '__main__':
-    HOST = ''
     PORT = 50001
+    HOST = ''
+    motd = 'Welcome to the server!'
+    motd_bytes = motd.encode()
 
+    print('[SERVER]\n')
     print('Starting server...')
-
     with socket.create_server((HOST, PORT)) as server:
         print('Server:', server.getsockname())
         server.listen()
         print('Server is now listening.')
-
         print("Awaiting connections...")
         conn, addr = server.accept()
-        with conn:
-            name = conn.recv(64)
-            print(addr, 'has connected as', name)
-            while True:
-                data = conn.recv(64)
-                if not data: break
-                decoded = data.decode()
-                print(decoded)
+        try:
+            with conn:
+                name = ''
+                name_bytes = conn.recv(64)
+                name = name_bytes.decode()
+                print(f'{addr} has connected as {name}.')
+                conn.sendall(motd_bytes)
+                while True:
+                    msg_bytes = conn.recv(64)
+                    if not msg_bytes: break
+                    msg = msg_bytes.decode()
+                    print(msg)
+        except Exception as e:
+            print(e)
+        finally:
+            if name:
+                print(f'{name} has disconnected.')
+            else:
+                print(f'{addr} has disconnected.')
     print('Server closed.\n')
